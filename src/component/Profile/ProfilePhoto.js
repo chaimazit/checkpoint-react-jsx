8446b3519dd49213dc9dde690145790a06aa70d9
chaimazit <zitounichaima2@gmail.com>
2023-03-16 19:33:14 +0100

    first commit

---
+++ src/component/Profile/ProfilePhoto.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const ProfilePhoto = () => {
+  return (
+    <div>
+    <img src='./photo chz.jpg' className="img-center" alt=""style={{width:'30%'}}/>
+    </div>
+
+  )
+}
+
+export default ProfilePhoto
